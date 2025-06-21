c044269c18697176b61737ff3d8e1bfa2b161476
JerryBS01 <139827565+JerryBS01@users.noreply.github.com>
2025-06-21 14:20:05 +1000

    Added google tag manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '../components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { Toaster } from 'react-hot-toast';
 import ThemeSwitch from '@/components/themeSwitch';
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,19 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WLTJBH6W');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-white dark:text-opacity-90`}>
         <div className="bg-[#a6dded] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#046263]"></div>
         <div className="bg-[#ebd59b] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
